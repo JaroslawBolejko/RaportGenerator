024c014058b3f24e163776514eff7030169e971b
LAPTOP-1A0N2URC\Bolejko <jaroslawbolejko85@gmail.com>
2022-10-21 12:51:52 +0200

    Add Main Page

--- RaportGenerator/MainWindow.xaml.cs
+++ RaportGenerator/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace RaportGenerator
         {
             InitializeComponent();
         }
+
+        private void generateRaport_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
