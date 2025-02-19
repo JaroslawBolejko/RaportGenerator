da2bfb652766b38edb921d014c752e95aaedc8f8
Jarosław <jaroslawbolejko85@gmail.com>
2025-02-19 20:45:31 +0100

    Add code behind for ReportModifyView
    To make this functionality work

--- RaportGenerator/Views/Report/ReportModifyView.xaml.cs
+++ RaportGenerator/Views/Report/ReportModifyView.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using RaportGenerator.Models;
+using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,6 +24,64 @@ namespace RaportGenerator.Views.Report
         public ReportModifyView()
         {
             InitializeComponent();
+        }
+
+        public ReportModel report;
+        public void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            setMonthItemSource();
+            cmbReportMonth.SelectedIndex = getInitialMonthIndex(report.ReportForMonth);
+            txtClientName.Text = report.CompanyName;
+            txtRaportNumber.Text = report.ReportNumber;
+            txtDeviceName.Text = report.DeviceName;
+            txtDeviceSerialNumber.Text = report.DeviceSerialNumber;
+            txtContractNumber.Text = report.ContractNumber;
+            txtPrevStateBlack.Text = report.PrevPrintsStateBlack.ToString();
+            txtPrevStateColor.Text = report.PrevPrintsStateColor.ToString();
+            txtCurrentStateBlack.Text = report.CurrentPrintsStateBlack.ToString();
+            txtCurrentStateColor.Text = report.CurrentPrintsStateColor.ToString();
+            txtCurrentPrintCounterBlack.Text = report.BlackPrintsAmount.ToString();
+            txtCurrentPrintCounterColor.Text = report.ColorPrintsAmount.ToString();
+            txtRaportOwner.Text = "Marek Jary";// docelowo admin
+            dpDateOfService.DataContext = new { ReportModel = new ReportModel { ReportDocumentDate = report.ReportDocumentDate } };
+        }
+
+        public void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void setMonthItemSource()
+        {
+            cmbReportMonth.ItemsSource = new CultureInfo("pl-PL").DateTimeFormat.MonthNames
+          .Where(m => !string.IsNullOrEmpty(m))
+          .ToList();
+        }
+
+        private int getInitialMonthIndex(string monthName)
+        {
+            int monthIndex = monthName switch
+            {
+                "styczeń" => 0,
+                "luty" => 1,
+                "marzec" => 2,
+                "kwiecień" => 3,
+                "maj" => 4,
+                "czerwiec" => 5,
+                "lipiec" => 6,
+                "sierpień" => 7,
+                "wrzesień" => 8,
+                "październik" => 9,
+                "listopad" => 10,
+                "grudzień" => 11,
+                _ => throw new ArgumentOutOfRangeException("Podany miesiąc nie istnieje!"),
+            };
+
+            return monthIndex;
         }
     }
 }
